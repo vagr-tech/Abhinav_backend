4141ba2e37599c70f54cbc4cc8eeefe58fabfc64
gokilavani-dev <gokilavani2k@gmail.com>
2026-07-02 10:50:21 +0530

    cache 3mins

--- zohoCacheJob.js
+++ zohoCacheJob.js
@@ -249,7 +249,7 @@ const buildZohoCache = async () => {
 };
 
 // ─── Cron: 2AM Daily ─────────────────────────────────
-cron.schedule("0 2 * * *", () => {
+cron.schedule("/3 * * * *", () => {
   console.log("🕛 Cron triggered at:", new Date().toISOString());
   buildZohoCache();
 });
